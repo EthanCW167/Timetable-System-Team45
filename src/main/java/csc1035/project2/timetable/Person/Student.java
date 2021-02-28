96fb211b09294fe1bc7b0b1f8976e1da8325256d
Ethan Wilson <E.Wilson17@newcastle.ac.uk>
2021-02-28 13:38:05 +0000

    Added constructor to Student inheriting from Person class

---
+++ src/main/java/csc1035/project2/timetable/Person/Student.java
@@ -0,0 +1,12 @@
+package csc1035.project2.timetable.Person;
+
+public class Student extends Person{
+
+    public Student(int id, String firstName, String lastName){
+
+        super(id,firstName,lastName);
+    }
+    public void returnStudentData(){
+
+    }
+}
