21100d00c62fd43aab388995ad3fcdac9f34f4eb
Rowan Lowe <lower123321@gmail.com>
2021-03-17 23:16:22 +0000

    Created Timetable class initialiser Created an override to print the Timetable as as String

--- src/main/java/csc1035/project2/timetable/Timetable.java
+++ src/main/java/csc1035/project2/timetable/Timetable.java
@@ -1,4 +1,32 @@
 package csc1035.project2.timetable;
 
+import csc1035.project2.booking.reservation.Reservations;
+
+import java.util.List;
+
 public class Timetable {
+    List<Reservations> reservationList;
+    public Timetable(List<Reservations> bookingsIn)
+    {
+        reservationList = bookingsIn;
+
+
+    }
+
+    @Override
+    public String toString() {
+        String stringFormat = "Timetable /n {";
+        for (Reservations r:reservationList)
+        {
+            stringFormat += r.toString() + "/n";
+
+        }
+
+        return "Timetable{" +
+
+                stringFormat +
+                '}';
+    }
+
+
 }
